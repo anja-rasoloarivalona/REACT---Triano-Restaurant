75b89ddcdafda23145db4a24ceb67e3b3caab835
Anja <rasoloanja@gmail.com>
2019-04-17 15:45:15 -0400

    Styled menu sections

--- src/Pages/Menu/Menu.js
+++ src/Pages/Menu/Menu.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import classes from './Menu.css';
+import {Link} from 'react-router-dom';
+import {Route} from 'react-router-dom';
+
 
 import Header from '../../components/Header/Header';
-import MenuItems from './MenuItems/MenuItems';
+import Lunch from './Lunch/Lunch';
+
 
 
 import background from '../../Assets/img/menuHeader.jpg';
@@ -17,107 +21,27 @@ const Menu = () => {
 
                 <section className={classes.Menu}>
 
-                  <section className={classes.MenuSection}>
+                  <nav className={classes.Navbar}>           
+                        <ul className={classes.List}>
+                            <li>
+                                <Link to="/menu" className={classes.Link}>LUNCH</Link>
+                            </li>
+                            <li>
+                                <Link to="/menu/diner" className={classes.Link}>DINER</Link>
+                            </li>
+                            <li>
+                                <Link to="/menu/brunch" className={classes.Link}>BRUNCH</Link>
+                            </li>
+                        </ul>
+                </nav>
+
+                <Route exact path="/menu/" component={Lunch}></Route>                     
+                <Route path="/menu/diner" component={Lunch}></Route> 
+                <Route path="/menu/brunch" component={Lunch}></Route>
+
                   
-                    <MenuItems 
 
-                    menuTitle = 'Starters'
-                    
-                    title1 = 'French onion soup'
-                    content1= 'Gratin with Gruyere Cheese'
-                    price1= '23'
-
-                    title2 = 'Escargot'
-                    content2= 'In a Parsley and Garlic Butter Sauce'
-                    price2= '33'
-
-                    title3 = 'Quiche of the day'
-                    content3= 'With Mesclun Salad on the side'
-                    price3= '15'
-
-                    title4 = 'Beef Tartar'
-                    content4= 'Shallots, Mustard, Chives, Sriracha and Parsley'
-                    price4= '27'
-
-                    title5 = 'Mesclun salad'
-                    content5= 'Dried Cranberries, Pistachios and a Balsamic Vinaigrette'
-                    price5= '14'                    
-                    />
-                  </section>
-
-                  <section className={classes.Parallax}>
-                      <section className={classes.ParallaxImgLeft}  style={{backgroundImage: " url(" + background + ")"}}/>
-                      <section className={classes.ParallaxImg}>
-                          <h3 className={classes.ParallaxTitle}>Our Chef</h3>
-                          <h3 className={classes.ParallaxSubtitle}>SELECTION</h3>
-                      </section>
-                      <section className={classes.ParallaxImgRight} style={{backgroundImage: " url(" + background + ")"}}/>
-                  </section>
-
-                <section className={classes.MenuSection}>
-                    <MenuItems 
-                    menuTitle = 'Main course'
-                    
-                    title1 = 'Salmon steak'
-                    content1= 'Creamy Dill Sauce and a side of Cous Cous with Merguez Sausage'
-                    price1= '30'
-
-                    title2 = 'Shrimp risotto'
-                    content2= 'Caramelized Fennel, Cherry Tomatoes, Lemon and a Saffron Foam'
-                    price2= '20'
-
-                    title3 = 'Pan seared trout'
-                    content3= 'In a Roasted Garlic and Herbs de Provençe'
-                    price3= '18'
-
-                    title4 = 'Pork medallions'
-                    content4= 'Stuffed with Granny Smith Apple and Rosemary'
-                    price4= '25'
-
-                    title5 = 'Caribean Lobster tail'
-                    content5= 'Brandy Beurre Noir with side of Haricot Verts Sauté'
-                    price5= '53'                    
-                    />
-                  </section>
-
-                  <section className={classes.Parallax}>
-                      <section className={classes.ParallaxImgLeft}  style={{backgroundImage: " url(" + background + ")"}}/>
-
-                      <section className={classes.ParallaxImg}>
-                          <h3 className={classes.ParallaxTitle}>Our Chef</h3>
-                          <h3 className={classes.ParallaxSubtitle}>SELECTION</h3>
-                      </section>
-
-                      <section className={classes.ParallaxImgRight} style={{backgroundImage: " url(" + background + ")"}}/>
-                  </section>
-
-                  <section className={classes.MenuSection}>
-                    <MenuItems 
-                    menuTitle = 'Desserts'
-                    
-                    title1 = 'Crêpe Suzette'
-                    content1= 'Orange Butter Sauce and Grand Marnier'
-                    price1= '13'
-
-                    title2 = 'Warm thin crust apple tart'
-                    content2= 'Apricot Glaze, Almond Paste and Vanilla Ice Cream'
-                    price2= '23'
-
-                    title3 = 'Quiche of the day'
-                    content3= 'With Mesclun Salad on the side'
-                    price3= '15'
-
-                    title4 = 'Dark chocolate mousse'
-                    content4= 'Perfumed with Rum Barrilito'
-                    price4= '11'
-
-                    title5 = 'Soufflé of the day'
-                    content5= 'Banana'
-                    price5= '10'                    
-                    />
-                </section>
-
-             </section>
+              </section>
 
   
 
