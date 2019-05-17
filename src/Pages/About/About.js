952083dbe83703766845434b42d9d4a0975bcc10
Anja <rasoloanja@gmail.com>
2019-05-17 19:40:27 -0400

    Init story section in about page

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
-import background from '../../Assets/img/aboutHeader.jpg';
+
+import TextRight from '../../components/UI/TextRight/TextRight';
+
+
+import background from '../../Assets/img/about/aboutHeader.jpg';
+import img1 from '../../Assets/img/home/chef.jpg';
+import img2 from '../../Assets/img/home/story.jpg';
+
+
+
 
 const About = () => {
   return (
@@ -8,6 +17,13 @@ const About = () => {
         <Header background={background}
                     text1 = "About"
                     text2= "US"/>
+
+        <TextRight
+                                twoImg
+                                title = "OUR STORY" 
+                                text = "Triano is a restaurant, bar and coffee roastery located on a busy corner site in Farringdon’s Exmouth Market. With glazed frontage on two sides of the building, overlooking the market and a bustling London intersection."
+                                img1 = {img1}
+                                img2 = {img2}/>
     </React.Fragment>
   )
 }
