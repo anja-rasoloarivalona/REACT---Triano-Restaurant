681b4a7dd0458d8da57f28ad53bbbddc207554a0
Anja <rasoloanja@gmail.com>
2019-05-18 16:56:01 -0400

    Added hover animation for button component

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Header from '../../components/Header/Header';
 import TextLeft from '../../components/UI/TextLeft/TextLeft';
 import TextRight from '../../components/UI/TextRight/TextRight';
@@ -30,67 +30,66 @@ import discover1 from '../../Assets/img/discover1.jpg';
 import discover2 from '../../Assets/img/discover2.jpg';
 
 
-const Home = () => {
-  return (
-    <React.Fragment>
 
-            <Header 
-                    text1 = "Welcome"
-                    text2 =  "THE TRIANO"
-                    background={homeHeader} />
-               
+class Home extends Component {
 
+        componentDidMount() {
+                window.scrollTo(0, 0)
+              }
+
+        render() {
+        return (
+        <React.Fragment>
+
+        <Header 
+                text1 = "Welcome"
+                text2 =  "THE TRIANO"
+                background={homeHeader} />
            
-            
-                        <TextLeft
-                                action = "Discover"
-                                twoImg
-                                title = "OUR STORY" 
-                                text = "Triano is a restaurant, bar and coffee roastery located on a busy corner site in Farringdon’s Exmouth Market. With glazed frontage on two sides of the building, overlooking the market and a bustling London intersection."
-                                img1 = {story}
-                                img2 = {chef}/>
-                    
 
-                        <FullParallax background = {diningRoom}
-                                      text = "The Dining Room"/>
-
-                        <TextRight 
-                                action = "Discover"
-                                title = "OUR MENU" 
-                                text = "Triano is a restaurant, bar and coffee roastery located on a busy corner site in Farringdon’s Exmouth Market. With glazed frontage on two sides of the building, overlooking the market and a bustling London intersection."
-                                
-                                img1 = {menu1}
-                                img2 = {menu2}
-                                img3 = {menu3}
-                                img4 = {menu4}
-                                
-                                path='/menu'
-                                buttonValue="Menu"/>
-                                
-
-                                <FullParallax background = {rooftop}
-                                      text = "The Rooftop"/>
-
-                        <TextLeft
-                                action = "Discover"
-                                twoImg
-                                title = "OUR BAR" 
-                                text = "Triano is a restaurant, bar and coffee roastery located on a busy corner site in Farringdon’s Exmouth Market. With glazed frontage on two sides of the building, overlooking the market and a bustling London intersection."
-                                img1 = {drinks1}
-                                img2 = {drinks2}
-                                        />
-                        
-                
-                               
-                    
-
-        
-
-
-    </React.Fragment>
        
-   
-  )
+        
+                    <TextLeft
+                            action = "Discover"
+                            twoImg
+                            title = "OUR STORY" 
+                            text = "Triano is a restaurant, bar and coffee roastery located on a busy corner site in Farringdon’s Exmouth Market. With glazed frontage on two sides of the building, overlooking the market and a bustling London intersection."
+                            img1 = {story}
+                            img2 = {chef}/>
+                
+
+                    <FullParallax background = {diningRoom}
+                                  text = "The Dining Room"/>
+
+                    <TextRight 
+                            action = "Discover"
+                            title = "OUR MENU" 
+                            text = "Triano is a restaurant, bar and coffee roastery located on a busy corner site in Farringdon’s Exmouth Market. With glazed frontage on two sides of the building, overlooking the market and a bustling London intersection."
+                            
+                            img1 = {menu1}
+                            img2 = {menu2}
+                            img3 = {menu3}
+                            img4 = {menu4}
+                            
+                            path='/menu'
+                            buttonValue="Menu"/>
+                            
+
+                            <FullParallax background = {rooftop}
+                                  text = "The Rooftop"/>
+
+                    <TextLeft
+                            action = "Discover"
+                            twoImg
+                            title = "OUR BAR" 
+                            text = "Triano is a restaurant, bar and coffee roastery located on a busy corner site in Farringdon’s Exmouth Market. With glazed frontage on two sides of the building, overlooking the market and a bustling London intersection."
+                            img1 = {drinks1}
+                            img2 = {drinks2}
+                                    />
+
+        </React.Fragment>
+    )
+  }
 }
 
 export default Home;
