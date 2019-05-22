def66f92afb651a33941c8000f062329df55dc00
Anja <rasoloanja@gmail.com>
2019-05-22 15:21:00 -0400

    Init happyHour section in bar page

--- src/Pages/Bar/HappyHour/HappyHour.js
+++ src/Pages/Bar/HappyHour/HappyHour.js
@@ -1,11 +1,67 @@
 import React from 'react';
 import classes from './HappyHour.css';
 
+import MenuItems from '../../Menu/MenuItems/MenuItems';
+
 const happyHour = () => {
     return (
-        <div>
-            Hello from Happy hour
-        </div>
+        <React.Fragment>
+
+            <section className={classes.Intro}>
+
+            </section>
+
+            <section className={classes.MenuSection}>
+                    <MenuItems 
+                        menuTitle = 'Cocktails'
+                        
+                        title1 = 'Spicy concombre'
+                        content1= 'Greenalls Gin, St.Germain, lime juice'
+                        price1= '21'
+
+                        title2 = 'Lolita'
+                        content2= 'Green Mark Vodka, Sparkling White Wine, grapfruit'
+                        price2= '15'
+
+                        title3 = 'Penicillin'
+                        content3= 'Famous Grouse Scotch, lemon juice'
+                        price3= '14'
+
+                        title4 = 'Midnight in Paris'
+                        content4= 'Park VS Cognac, Green Mark Vodka, lemon, orange'
+                        price4= '27'
+
+                        title5 = 'Light and Balmy'
+                        content5= 'Sailor Jerry rum, lime juice, ginger beer'
+                        price5= '14'               
+                    />
+            </section>
+            <section className={classes.MenuSection}>
+                    <MenuItems 
+                        menuTitle = 'Wine'
+                        
+                        title1 = 'Chardonnay, Martin Ray'
+                        content1= 'Russian River Valley, California'
+                        price1= '13'
+
+                        title2 = 'Pinot Grigio'
+                        content2= 'Veneto, Italy'
+                        price2= '15'
+
+                        title3 = 'Sauvignon Blanc'
+                        content3= 'Napa Valley, California'
+                        price3= '15'
+
+                        title4 = 'Côte du Rhône, Mont-Redon'
+                        content4= 'Côtes du Rhône, France'
+                        price4= '27'
+
+                        title5 = 'Bordeaux, Chateau de Sours'
+                        content5= 'Bordeaux, France'
+                        price5= '14'               
+                    />
+            </section>
+        </React.Fragment>
     )
 }
 
