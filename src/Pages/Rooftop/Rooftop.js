b50cf44aee0cc3ca20a553467a32c34c523c853f
Anja <rasoloanja@gmail.com>
2019-05-21 21:22:26 -0400

    Init Rooftop page

--- src/Pages/Rooftop/Rooftop.js
+++ src/Pages/Rooftop/Rooftop.js
@@ -1,24 +1,17 @@
 import React from 'react';
+import classes from './Rooftop.css';
 import Header from '../../components/Header/Header';
-import background from '../../Assets/img/rooftopHeader.jpg';
+
+import background from '../../Assets/img/rooftop/rooftopHeader.jpg';
 
 import TextLeft from '../../components/UI/TextLeft/TextLeft';
-import story from '../../Assets/img/home/story.jpg';
-import chef from '../../Assets/img/home/chef.jpg';
 
 const Rooftop = () => {
   return (
     <React.Fragment>
-        <Header background={background}
-                    text1 = "Our"
-                    text2= "ROOFTOP"/>
-          
-          <TextLeft
-                                twoImg
-                                title = "OUR STORY" 
-                                text = "Triano is a restaurant, bar and coffee roastery located on a busy corner site in Farringdon’s Exmouth Market. With glazed frontage on two sides of the building, overlooking the market and a bustling London intersection."
-                                img1 = {story}
-                                img2 = {chef}/>
+        <section className={classes.Intro}>
+
+        </section>
     </React.Fragment>
   )
 }
