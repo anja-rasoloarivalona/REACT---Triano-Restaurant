d1ee41c7cf2957eb6c1974360c026fe32b5b4fc5
Anja <rasoloanja@gmail.com>
2019-05-31 18:21:29 -0400

    Built project and deployed with firebase

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import SideDrawer from './components/Navbar/SideDrawer/SideDrawers';
 import LogoContainer from './components/LogoContainer/LogoContainer';
 import Footer from './components/Footer/Footer';
 import Spinner from './components/Spinner/Spinner';
-import {Route, Redirect } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 import Home from './Pages/Home/Home';
 
@@ -56,6 +56,7 @@ class App extends Component {
           <LogoContainer />
           <SideDrawer />
           <Navbar />
+          <Switch>
             <Route exact path="/" component={Home}></Route>                     
             <Route path="/about" component={lazyAbout}></Route> 
             <Route path="/menu" component={lazyMenu}></Route>
@@ -63,6 +64,7 @@ class App extends Component {
             <Route path="/bar" component={lazyBar}></Route>
             <Route path="/reservation" component={lazyReservation}></Route>
             <Route component={Home}/>
+            </Switch>
           <Footer />   
       </AppLayout>
     );
