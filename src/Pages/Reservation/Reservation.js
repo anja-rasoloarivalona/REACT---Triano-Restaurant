8a1e55af948c2ae561a796eb3b5f544aceb635b0
Anja <rasoloanja@gmail.com>
2019-05-31 11:16:17 -0400

    Format reservation and rooftop components

--- src/Pages/Reservation/Reservation.js
+++ src/Pages/Reservation/Reservation.js
@@ -1,75 +1,75 @@
-import React, { Component } from 'react';
-import classes from './Reservation.css';
-import Header from '../../components/Header/Header';
-import background from '../../Assets/img/about/aboutHeader.jpg';
+import React, { Component } from "react";
+import classes from "./Reservation.css";
 
-import entry from '../../Assets/img/entry.jpg';
-import Parallax from '../../components/UI/Gallery/FullParallax/FullParallax';
+import Header from "../../components/Header/Header";
+import Parallax from "../../components/UI/Gallery/FullParallax/FullParallax";
 
-import diningRoom from '../../Assets/img/home/dining-room.png';
-import rooftop from '../../Assets/img/home/rooftop.jpg';
+/* -------IMG ------------ */
+import background from "../../Assets/img/about/aboutHeader.jpg";
+import entry from "../../Assets/img/entry.jpg";
+import diningRoom from "../../Assets/img/home/dining-room.png";
+import rooftop from "../../Assets/img/home/rooftop.jpg";
 
 class Reservation extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
 
-    componentDidMount() {
-        window.scrollTo(0, 0)
-      }
+  render() {
+    return (
+      <React.Fragment>
+        <Header background={background} text1="Book" text2="YOUR TABLE" />
 
+        <section className={classes.Entry}>
+          <div className={classes.EntryContainer}>
+            <img src={entry} className={classes.EntryImg} />
+          </div>
 
-    render() {
-        return (
-            <React.Fragment>
-                <Header background={background}
-                    text1 = "Book"
-                    text2= "YOUR TABLE"/>
+          <div className={classes.Text}>
+            <h3 className={classes.Title}>Reservation</h3>
+            <p className={classes.Para}>
+              Christian Le Squer and his team will be delighted to welcome you
+              at Le Cinq Restaurant. To prepare your event and guarantee you a
+              personalised welcome, we recommend you reserve a table. Le Cinq
+              restaurant is open every day from 12.30 to 2.00pm for lunch, and
+              from 7.00 to 9.30pm for dinner.
+            </p>
+            <div />
+          </div>
+        </section>
 
+        <Parallax background={background} text="Choose Your table" />
 
-                <section className={classes.Entry}>
-                        <div className={classes.EntryContainer}>
-                            <img src={entry} className={classes.EntryImg}/>
-                        </div>
-                        
-                        <div className={classes.Text}>
-                            <h3 className={classes.Title}>Reservation</h3>
-                            <p className={classes.Para}>
-                                Christian Le Squer and his team will be delighted to welcome you at Le Cinq Restaurant. To prepare your event and guarantee you a personalised welcome, we recommend you reserve a table. Le Cinq restaurant is open every day from 12.30 to 2.00pm for lunch, and from 7.00 to 9.30pm for dinner.
-                            </p> 
-                         <div/>
-                                                     
-                    </div> 
-                     
-                </section>
+        <section className={classes.Table}>
+          <div className={classes.TableItem}>
+            <img src={diningRoom} className={classes.TableImg} alt="Table" />
+            <div className={classes.TableTitle}>The Dining Room</div>
+          </div>
+          <div className={classes.TableItem}>
+            <img src={rooftop} className={classes.TableImg} alt="Table" />
+            <div className={classes.TableTitle}>The Rooftop</div>
+          </div>
+        </section>
 
-                <Parallax background = {background}
-                                  text = "Choose Your table"/>
-
-                <section className={classes.Table}>
-                    <div className={classes.TableItem}>
-                        <img src={diningRoom} className={classes.TableImg} alt='Table' />
-                        <div className={classes.TableTitle}>The Dining Room</div>
-                    </div>
-                    <div className={classes.TableItem}>
-                        <img src={rooftop} className={classes.TableImg} alt='Table' />
-                        <div className={classes.TableTitle}>The Rooftop</div>
-                    </div>
-                    
-                </section>
-
-                <section className={classes.Contact}>
-                    <div className={classes.ContactContainer}>
-                        <div className={classes.ContactTitle}>Contact</div>
-                        <div className={classes.ContactItem}>
-                                <span className={classes.ContactItemContent}><b>Tel : </b>+1 514 1234 123</span>
-                                <span className={classes.ContactItemContent}><b>E-mail : </b>triano_restaurant@mymail.com</span>
-                                <span className={classes.ContactItemContent}><b>Adress : </b>54 Avenue Colin, Montréal, QC CANADA</span>
-                        </div>
-                    </div>
-                        
-                </section>
-            </React.Fragment>
-
-        )
-    }
+        <section className={classes.Contact}>
+          <div className={classes.ContactContainer}>
+            <div className={classes.ContactTitle}>Contact</div>
+            <div className={classes.ContactItem}>
+              <span className={classes.ContactItemContent}>
+                <b>Tel : </b>+1 514 1234 123
+              </span>
+              <span className={classes.ContactItemContent}>
+                <b>E-mail : </b>triano_restaurant@mymail.com
+              </span>
+              <span className={classes.ContactItemContent}>
+                <b>Adress : </b>54 Avenue Colin, Montréal, QC CANADA
+              </span>
+            </div>
+          </div>
+        </section>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Reservation;
--- src/Pages/Rooftop/Rooftop.js
+++ src/Pages/Rooftop/Rooftop.js
@@ -1,106 +1,94 @@
-import React, { Component } from 'react'
-import classes from './Rooftop.css';
+import React, { Component } from "react";
+import classes from "./Rooftop.css";
 
-import Header from '../../components/Header/Header';
-import Button from '../../components/Button/Button';
-import Parallax from '../../components/UI/Gallery/FullParallax/FullParallax';
+import Header from "../../components/Header/Header";
+import Button from "../../components/Button/Button";
+import Parallax from "../../components/UI/Gallery/FullParallax/FullParallax";
 
-import background from '../../Assets/img/rooftop/rooftopHeader.jpg';
-import rooftop from '../../Assets/img/rooftop/rooftop.jpg';
-
-import brunch from '../../Assets/img/rooftop/brunch.jpeg';
-import happy from '../../Assets/img/rooftop/happy.jpeg';
-
-
-
+/*-----------IMG-----------*/
+import background from "../../Assets/img/rooftop/rooftopHeader.jpg";
+import rooftop from "../../Assets/img/rooftop/rooftop.jpg";
+import brunch from "../../Assets/img/rooftop/brunch.jpeg";
+import happy from "../../Assets/img/rooftop/happy.jpeg";
 
 class Rooftop extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
 
-    componentDidMount() {
-        window.scrollTo(0, 0)
-      }
+  render() {
+    return (
+      <React.Fragment>
+        <Header background={background} text1="Our" text2="ROOFTOP" />
 
+        <section className={classes.Intro}>
+          <img src={rooftop} className={classes.IntroImg} alt="rooftop" />
 
-    render() {
-        return (
-            <React.Fragment>
+          <div className={classes.IntroTextContainer}>
+            <div className={classes.IntroTitle}>
+              <span>The</span>
+              <span>Rooftop Lounge</span>
+            </div>
+            <p>
+              Our Rooftop Lounge is Montreal's largest outdoor Rooftop Garden
+              and Fully enclosed Penthouse Lounge. One floor is fully enclosed.
+              And our rooftop garden is open to the sky. We do have large
+              umbrellas for those sunny or rainy days and is partially heated on
+              colder nights.
+            </p>
 
-            <Header background={background}
-                    text1= 'Our'
-                    text2= 'ROOFTOP'
-                    />
-    
-            <section className={classes.Intro}>
-              
-                      <img src={rooftop} className={classes.IntroImg} alt='rooftop'/>
-               
-                <div className={classes.IntroTextContainer}>
-                    <div className={classes.IntroTitle}>
-                        <span>The</span>
-                        <span>Rooftop Lounge</span>
-                    </div>
-                    <p>
-                    Our Rooftop Lounge is Montreal's largest outdoor Rooftop Garden and Fully enclosed Penthouse Lounge. One floor is fully enclosed. And our rooftop garden is open to the sky. We do have large umbrellas for those sunny or rainy days and is partially heated on colder nights.
-                    </p>
-    
-                    <Button 
-                    path='/reservation'
-                    buttonValue='Book a table'/>
-    
-                </div>        
-            </section>
-    
-            <Parallax background={background}
-                      text="Our events"/>
-    
-    
-            <section className={classes.Event}>
-              <div className={classes.EventContainer}>
-    
-                  <div className={classes.EventItem}
-                  style={{backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)), url(" + brunch + ")",
-                  backgroundPosition: "center",
-                  backgroundRepeat: "no-repeat",
-                  backgroundSize: "cover"}}>
-                        <div className={classes.EventTitle}>
-                            Brunch Buffet
-                        </div>
-                        <div className={classes.EventDay}> 
-                            Saturdays & Sundays
-                        </div>
-                        <div className={classes.EventHour}>
-                            <i>10am-4pm</i>
-                        </div>
-                        <Button 
-                            path='/menu/brunch'
-                            buttonValue='Learn more'/>
-                  </div>
-    
-                  <div className={classes.EventItem}
-                  style={{backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)), url(" + happy+ ")",
-                  backgroundPosition: "center",
-                  backgroundRepeat: "no-repeat",
-                  backgroundSize: "cover"}}>>
-                        <div className={classes.EventTitle}>
-                            Happy Hour
-                        </div>
-                        <div className={classes.EventDay}> 
-                            Monday-Friday
-                        </div>
-                        <div className={classes.EventHour}>
-                            <i>4pm-7pm</i>
-                        </div>
-                        <Button 
-                            path='/bar/happyHour'
-                            buttonValue='Learn more'/>
-                  </div>
+            <Button path="/reservation" buttonValue="Book a table" />
+          </div>
+        </section>
+
+        <Parallax background={background} text="Our events" />
+
+        <section className={classes.Event}>
+          <div className={classes.EventContainer}>
+            <div
+              className={classes.EventItem}
+              style={{
+                backgroundImage:
+                  "linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)), url(" +
+                  brunch +
+                  ")",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover"
+              }}
+            >
+              <div className={classes.EventTitle}>Brunch Buffet</div>
+              <div className={classes.EventDay}>Saturdays & Sundays</div>
+              <div className={classes.EventHour}>
+                <i>10am-4pm</i>
               </div>
-    
-            </section>
-        </React.Fragment>
-        )
-    }
-}
+              <Button path="/menu/brunch" buttonValue="Learn more" />
+            </div>
 
+            <div
+              className={classes.EventItem}
+              style={{
+                backgroundImage:
+                  "linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)), url(" +
+                  happy +
+                  ")",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover"
+              }}
+            >
+              ><div className={classes.EventTitle}>Happy Hour</div>
+              <div className={classes.EventDay}>Monday-Friday</div>
+              <div className={classes.EventHour}>
+                <i>4pm-7pm</i>
+              </div>
+              <Button path="/bar/happyHour" buttonValue="Learn more" />
+            </div>
+          </div>
+        </section>
+      </React.Fragment>
+    );
+  }
+}
 
 export default Rooftop;
